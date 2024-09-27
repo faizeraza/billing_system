ca04a3f0859e0822ea7f9cbd5b9ff22730071c62
Mohammad faizan <faizeraza4us@gmail.com>
2024-09-27 17:03:32 +0530

    added ui to thymeleaf

--- billing_system/src/main/java/com/example/billing_system/controller/OrderController.java
+++ billing_system/src/main/java/com/example/billing_system/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.example.billing_system.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import com.example.billing_system.entities.Invoice;
 import com.example.billing_system.service.GenerateBill;
 
 @RestController
+
 @RequestMapping("/order")
 public class OrderController {
 
@@ -17,6 +20,11 @@ public class OrderController {
 
     public OrderController(GenerateBill generateBill1) {
         this.generateBill = generateBill1;
+    }
+
+    @GetMapping("/")
+    public String getMethodName(Model model) {
+        return "index";
     }
 
     @PostMapping("/newBill")
